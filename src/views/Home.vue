db0aae717022c078a02bbcdd158ee4562101683f
Pavel Tigiev <paulo.tigiev@gmail.com>
2021-10-24 02:48:53 +0300

    add icons to dashboard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,84 +33,84 @@
     <div class="nav-grid" role="grid">
       <el-row :gutter="0">
         <el-col :span="6" class="grid-item">
-          <div class="grid-inner">
-            <figure class="index_helpers-grid_icon">
+          <a href="/companies" class="grid-inner">
+            <figure class="icon-box">
               <svg width="34" height="48">
-                <use xlink:href="/static/img/svg-sprite/svg-symbols.svg#rocket"></use>
+                <use xlink:href="/img/svg-symbols.svg#rocket"></use>
               </svg>
             </figure>
             <h5 class="inner_title">Найти стартапы<br> и&nbsp;технологические<br> компании</h5>
-          </div>
+          </a>
         </el-col>
         <el-col :span="6" class="grid-item">
-            <div class="grid-inner">
-              <figure class="index_helpers-grid_icon">
-                <svg width="44" height="46">
-                  <use xlink:href="/static/img/svg-sprite/svg-symbols.svg#investments"></use>
+          <a href="/companies" class="grid-inner">
+            <figure class="icon-box">
+              <svg width="44" height="46">
+                <use xlink:href="/img/svg-symbols.svg#investments"></use>
+              </svg>
+            </figure>
+            <h5 class="inner_title">Где получить<br> инвестиции</h5>
+          </a>
+        </el-col>
+        <el-col :span="6" class="grid-item">
+          <a href="/companies" class="grid-inner">
+            <figure class="icon-box">
+              <svg width="44" height="50">
+                <use xlink:href="/img/svg-symbols.svg#market-box"></use>
+              </svg>
+            </figure>
+            <h5 class="inner_title">КАК ВЫЙТИ НА РЫНОК<br> И ГДЕ НАЙТИ КЛИЕНТОВ</h5>
+          </a>
+        </el-col>
+        <el-col :span="6" class="grid-item">
+            <a href="/companies" class="grid-inner">
+              <figure class="icon-box">
+                <svg width="40" height="36">
+                  <use xlink:href="/img/svg-symbols.svg#rooms"></use>
                 </svg>
               </figure>
-              <h5 class="inner_title">Где получить<br> инвестиции</h5>
-            </div>
+              <h5 class="inner_title">ГДЕ НАЙТИ<br> ПОМЕЩЕНИЕ ДЛЯ<br> ОФИСА/РАБОЧЕЕ МЕСТО</h5>
+            </a>
         </el-col>
         <el-col :span="6" class="grid-item">
-            <div class="grid-inner">
-              <figure class="index_helpers-grid_icon">
-                <svg width="44" height="50">
-                    <use xlink:href="/static/img/svg-sprite/svg-symbols.svg#market-box"></use>
-                </svg>
-              </figure>
-              <h5 class="inner_title">КАК ВЫЙТИ НА РЫНОК<br> И ГДЕ НАЙТИ КЛИЕНТОВ</h5>
-            </div>
+          <a href="/companies" class="grid-inner">
+            <figure class="icon-box">
+              <svg width="37" height="37">
+                <use xlink:href="/img/svg-symbols.svg#prototype"></use>
+              </svg>
+            </figure>
+            <h5 class="inner_title">ГДЕ ДОРАБОТАТЬ<br> ПРОТОТИП</h5>
+          </a>
         </el-col>
         <el-col :span="6" class="grid-item">
-              <div class="grid-inner">
-                  <figure class="index_helpers-grid_icon">
-                      <svg width="40" height="36">
-                          <use xlink:href="/static/img/svg-sprite/svg-symbols.svg#rooms"></use>
-                      </svg>
-                  </figure>
-                  <h5 class="inner_title">ГДЕ НАЙТИ<br> ПОМЕЩЕНИЕ ДЛЯ<br> ОФИСА/РАБОЧЕЕ МЕСТО</h5>
-              </div>
+          <a href="/companies" class="grid-inner">
+            <figure class="icon-box">
+              <svg width="38" height="38">
+                <use xlink:href="/img/svg-symbols.svg#expand"></use>
+              </svg>
+            </figure>
+            <h5 class="inner_title">ГДЕ ПРОКАЧАТЬ<br> КОМПЕТЕНЦИИ/<br> РАСШИРИТЬ НЕТВОРК</h5>
+          </a>
         </el-col>
         <el-col :span="6" class="grid-item">
-              <div class="grid-inner">
-                  <figure class="index_helpers-grid_icon">
-                      <svg width="37" height="37">
-                          <use xlink:href="/static/img/svg-sprite/svg-symbols.svg#prototype"></use>
-                      </svg>
-                  </figure>
-                  <h5 class="inner_title">ГДЕ ДОРАБОТАТЬ<br> ПРОТОТИП</h5>
-              </div>
+          <a href="/companies" class="grid-inner">
+            <figure class="icon-box">
+              <svg width="44" height="42">
+                <use xlink:href="/img/svg-symbols.svg#question"></use>
+              </svg>
+            </figure>
+            <h5 class="inner_title">ГДЕ ПОЛУЧИТЬ<br> КОНСУЛЬТАЦИЮ<br> ПО БИЗНЕС-ВОПРОСАМ</h5>
+          </a>
         </el-col>
         <el-col :span="6" class="grid-item">
-              <div class="grid-inner">
-                  <figure class="index_helpers-grid_icon">
-                      <svg width="38" height="38">
-                          <use xlink:href="/static/img/svg-sprite/svg-symbols.svg#expand"></use>
-                      </svg>
-                  </figure>
-                  <h5 class="inner_title">ГДЕ ПРОКАЧАТЬ<br> КОМПЕТЕНЦИИ/<br> РАСШИРИТЬ НЕТВОРК</h5>
-              </div>
-        </el-col>
-        <el-col :span="6" class="grid-item">
-              <div class="grid-inner">
-                  <figure class="index_helpers-grid_icon">
-                      <svg width="44" height="42">
-                          <use xlink:href="/static/img/svg-sprite/svg-symbols.svg#question"></use>
-                      </svg>
-                  </figure>
-                  <h5 class="inner_title">ГДЕ ПОЛУЧИТЬ<br> КОНСУЛЬТАЦИЮ<br> ПО БИЗНЕС-ВОПРОСАМ</h5>
-              </div>
-        </el-col>
-        <el-col :span="6" class="grid-item">
-            <div class="grid-inner">
-              <figure class="index_helpers-grid_icon">
-                <svg width="35" height="35">
-                  <use xlink:href="/static/img/svg-sprite/svg-symbols.svg#checklist"></use>
-                </svg>
-              </figure>
-              <h5 class="inner_title">ГДЕ ПРОТЕСТИРОВАТЬ<br> ПРОДУКТ</h5>
-            </div>
+          <a href="/companies" class="grid-inner">
+            <figure class="icon-box">
+              <svg width="35" height="35">
+                <use xlink:href="/img/svg-symbols.svg#checklist"></use>
+              </svg>
+            </figure>
+            <h5 class="inner_title">ГДЕ ПРОТЕСТИРОВАТЬ<br> ПРОДУКТ</h5>
+          </a>
         </el-col>
       </el-row>
     </div>
@@ -119,8 +119,6 @@
 </template>
 
 <script>
-// import Programs from '../components/Programs.vue'
-
 export default {
   name: 'Home'
 }
@@ -158,13 +156,35 @@ export default {
       color: #fff;
 
       .grid-inner {
+        display: block;
+        color: #fff;
         height: 290px;
         padding: 3rem 3rem 2.5rem;
+        text-decoration: none;
 
         &:hover {
           color: #fff;
           background-color: #FF1F55;
           cursor: pointer;
+        }
+
+        .icon-box {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 5.25rem;
+          height: 5.25rem;
+          border: 1px solid rgba(255, 255, 255, 0.15);
+          border-radius: 1rem;
+          margin-left: 0;
+          margin-bottom: 2rem;
+          transition: border-color .2s ease-out, box-shadow .2s ease-out, transform .2s ease-out;
+          will-change: border-color, box-shadow, transform;
+
+          svg {
+            fill: none;
+            stroke: #fff;
+          }
         }
 
         .inner_title {
